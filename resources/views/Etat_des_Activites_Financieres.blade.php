3f93d27cac7d993b48e7356b477838e87328d540
Alpha88 <aignondelofondimitri@gmail.com>
2024-03-05 15:38:32 +0100

    Etat des activites Financieres sans connexion a la base de donnee

--- resources/views/Etat_des_Activites_Financieres.blade.php
+++ resources/views/Etat_des_Activites_Financieres.blade.php
@@ -11,51 +11,363 @@
 <body>
 
 <div class="container">
-  <h2>Contextual Classes</h2>
-  <p>Contextual classes can be used to color table rows or table cells. The classes that can be used are: .active, .success, .info, .warning, and .danger.</p>
-  <table class="table">
+  <center>
+    <h3><u>SOS Villages d'enfants BENIN</u></h3>
+    <h3><u>Etat des Activités Financières pour l'exercice en cours qui se termine le 31 Décembre 2021</u> </h3>
+  </center>
+  <br>
+  <table class="table table-bordered" >
     <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
+      <tr class="success">
+        <td></td>
+        <th>Notes</th>
+        <th></th>
+        <th>Exercice en cours</th>
+        <th></th>
+        <th></th>
+        <th>Exercice précédent</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
+      <tr class="success">
+        <td></td>
+        <td></td>
+        <td>Non Restreints</td>
+        <td>Restreints</td>
+        <td>Total</td>
+        <td>Non Restreints</td>
+        <td>Restreints</td>
+        <td>Total</td>
+      </tr>     
+      <tr class="success">
+        <td>RECETTES</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr class="success">
+        <td>Recettes de l'étranger</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
       <tr>
-        <td>Default</td>
-        <td>Defaultson</td>
-        <td>def@somemail.com</td>
+        <td>Recettes des APS pour RC</td>
+        <td>1</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Recettes des APS pour CC</td>
+        <td>2</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr class="success">
+        <td>Recettes Locales</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Recettes Locales Collecte de Fonds (Fundraising)</td>
+        <td>3</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Recettes Locales des Subventions Gouvernementales</td>
+        <td>4</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Recettes d'Exploitation</td>
+        <td>5</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Autres Recettes Locales</td>
+        <td>6</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr class="warning">
+        <td>Total des Recettes</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>    
+      <tr >
+        <td><br></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>    
+      <tr class="success">
+        <td>DEPENSES</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Dépenses Projet/Programme</td>
+        <td>7</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Dépenses Administratives</td>
+        <td>8</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Dépenses de Collecte de Fonds (Fundraising)</td>
+        <td>9</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Autres Dépenses</td>
+        <td>10</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr class="warning">
+        <td>Total des Dépenses</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td><br></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr class="warning">
+        <td>Excédent/déficit net sur les activités opérationnelles</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
       </tr>      
       <tr class="success">
-        <td>Success</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr class="danger">
-        <td>Danger</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr class="info">
-        <td>Info</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
+        <td>Excédent/déficit net sur les activités opérationnelles</td>
+        <td>11</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>  
+      <tr>
+        <td>Intérêts créditeurs (recettes)</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>   
+      <tr>
+        <td>Intérêts débiteurs (dépenses)</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Autres recettes financières</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td>Autres dépenses financières</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr class="success">
+        <td>Gains ou pertes résultant du Taux de change</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
       <tr class="warning">
-        <td>Warning</td>
-        <td>Refs</td>
-        <td>bo@example.com</td>
-      </tr>
-      <tr class="active">
-        <td>Active</td>
-        <td>Activeson</td>
-        <td>act@example.com</td>
-      </tr>
+        <td>Excédent/déficit net avant impôts</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr>
+        <td><br></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>  
+      <tr class="success">
+        <td>Charges Fiscales</td>
+        <td>12</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>  
+      <tr>
+        <td><br></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
+      <tr class="warning">
+        <td>Excédent/déficit net après impôts</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr> 
     </tbody>
   </table>
 </div>
-
+<br><br>
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div style="text-align:center"class="card-body">
+        <h5 class="card-title">Directrice Finances et Contrôle</h5>
+        <br><br>
+        <p class="card-text">Diene ALIOUNE</p>
+       
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div style="text-align:center" class="card-body">
+        <h5 class="card-title">Directeur International Région</h5>
+        <br><br>
+        <p class="card-text">Benoît PIOT</p>
+       
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<br>
 </body>
 </html>
 
