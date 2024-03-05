7eb017f784d002d3765bc9ade25ff59e3a3895cb
MikHng <161308121+MikHng@users.noreply.github.com>
2024-03-05 15:59:35 +0100

    notes EAF fait sans integration des informations de la base de données

--- resources/views/Notes_EAF.blade.php
+++ resources/views/Notes_EAF.blade.php
@@ -16,7 +16,7 @@
     <h4><u>Notes à l'Etat des Activités Financières de l'exercice clos au 31 Décembre 2021 </u></h4>
     <br><br>
     <h5>Note 1 : Recettes des APS pour Dépenses de Fonctionnement (RC)</h5>
- </center>
+  </center>
   <table class="table table-bordered" >
     <thead>
       <tr class="success">
@@ -129,11 +129,11 @@
         <td>-</td>
         <td>98565625</td>
       </tr>
-      <br><br>git status
-
- <center>
+  </table>
+      <br><br>
+  <center>
       <h5>Note 2 : Recettes des APS pour Dépenses de Construction (CC)</h5>
- </center>
+  </center>
   <table class="table table-bordered" >
     <thead>
       <tr class="success">
@@ -159,11 +159,11 @@
       <tr>
         <td>SOS Kinderdorf e.V.</td>
         <td>52654125</td>
-        <td></td>
         <td>-</td>
+        <td>52654125</td>
+        <td>52654125</td>
         <td>-</td>
-        <td>-</td>
-        <td>-</td>
+        <td>52654125</td>
       </tr>
       <tr>
         <td>SOS Denmark</td>
@@ -184,46 +184,37 @@
         <td>-</td>
       </tr>
       <tr>
-        <td>Transfer fr. SOS Ass. via STS</td>
-        <td>98565625</td>
-        <td></td>
-        <td>98565625</td>
-        <td>98565625</td>
+        <td>SOS Norway</td>
         <td>-</td>
-        <td>98565625</td>
+        <td></td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>SOS Netherlands</td>
+        <td>-</td>
+        <td></td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Subsidies for CC SOS-FR</td>
+        <td>-</td>
+        <td></td>
+        <td></td>
+        <td>98598565</td>
+        <td>-</td>
+        <td>45122115</td>
       </tr>
       <tr>
         <td>...</td>
         <td>-</td>
-        <td></td>
         <td>-</td>
         <td>-</td>
-        <td>-</td>
-        <td>-</td>
-      </tr>
-      <tr>
-        <td>Dons spéciaux via le HGFD</td>
-        <td>-</td>
-        <td></td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-      </tr>
-      <tr>
-        <td>Dons spéciaux via SOS-KDEV</td>
-        <td>-</td>
-        <td></td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-      </tr>     
-      <tr>
-        <td>...</td>
-        <td>98565625</td>
-        <td></td>
-        <td>98565625</td>
         <td>-</td>
         <td>-</td>
         <td>-</td>
@@ -237,91 +228,585 @@
         <td>-</td>
         <td>98565625</td>
       </tr>
-
-
-
-
-
-
+  </table>
+  <br><br>
+  <center>
+  <h5>Note 3 : Recettes de Collecte de Fonds (Fundraising)</h5>
+  </center>
+  <table class="table table-bordered" >
+    <thead>
+      <tr class="success">
+        <td></td>
+        <th>Eec Non Restreints</th>
+        <th>Eec Restreints</th>
+        <th>Eec Total</th>
+        <th>Ep Non Restreints</th>
+        <th>Ep Restreints</th>
+        <th>Ep Total</th>
+      </tr>
+    </thead>
+    <tbody>
       <tr>
-        <td>Structure 1</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td>0</td>
+        <td>Dons en général & cadeaux individuels</td>
+        <td>95624</td>
+        <td>-</td>
+        <td>851412</td>
+        <td>9562314</td>
+        <td>-</td>
+        <td>956244</td>
       </tr>
       <tr>
-        <td>Structure 2</td>
+        <td>Parrains locaux/Dons réguliers</td>
+        <td>95624</td>
+        <td>-</td>
+        <td>851412</td>
+        <td>9562314</td>
+        <td>-</td>
+        <td>956244</td>
+      </tr>
+      <tr>
+        <td>Entreprises & grands donateurs </td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Legs &  Héritages</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Fondations & autres institutions</td>
+        <td>95624</td>
+        <td>-</td>
+        <td>851412</td>
+        <td>9562314</td>
+        <td>-</td>
+        <td>956244</td>
+      </tr>
+      <tr>
+        <td>Manifestations</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Marchandisage & Concession de licence</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Dons en nature de source privée</td>
+        <td>95624</td>
+        <td>-</td>
+        <td>851412</td>
+        <td>9562314</td>
+        <td>-</td>
+        <td>956244</td>
+      </tr>
+      <tr>
+        <td>Dons directs de l'étranger</td>
+        <td>95624</td>
+        <td>-</td>
+        <td>851412</td>
+        <td>9562314</td>
+        <td>-</td>
+        <td>956244</td>
+      </tr> 
+      <tr class="warning">
+        <td>Total</td>
+        <td>98565625</td>
+        <td>-</td>
+        <td>98565625</td>
+        <td>98565625</td>
+        <td>-</td>
+        <td>98565625</td>
+      </tr>
+  </table>
+  <br><br>
+  <center>
+  <h5>Note 4 : Recettes Locales du Gouvernement et Autres </h5>
+  </center>
+  <table class="table table-bordered" >
+    <thead>
+      <tr class="success">
         <td></td>
-        <td></td>
-        <td></td>
-        <td>0</td>
+        <th>Eec Non Restreints</th>
+        <th>Eec Restreints</th>
+        <th>Eec Total</th>
+        <th>Ep Non Restreints</th>
+        <th>Ep Restreints</th>
+        <th>Ep Total</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Subventions du gouvernement</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Dons en nature de source publique</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
       </tr>
       <tr class="warning">
         <td>Total</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+  </table>
+  <br><br>
+  <center>
+  <h5>Note 5 : Recettes d'Exploitation </h5>
+  </center>
+  <table class="table table-bordered" >
+    <thead>
+      <tr class="success">
+        <td></td>
+        <th>Exercice en cours</th>
+        <th>Exercice précédent</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Cotisations</td>
+        <td>895645544</td>
+        <td>895645254</td>
       </tr>
       <tr>
-        <td><br></td>
+        <td>Recettes</td>
+        <td>87451221</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Cotisations d'autres institutions SOS</td>
+        <td>5412632</td>
+        <td>5642632</td>
+      </tr>
+      <tr class="warning">
+      <td>Total</td>
+        <td>98652756</td>
+        <td>4512426</td>
+      </tr>
+  </table>
+  <br><br>
+  <center>
+  <h5>Note 6 : Autres Recettes Locales </h5>
+  </center>
+  <table class="table table-bordered" >
+    <thead>
+      <tr class="success">
+        <td></td>
+        <th>Exercice en cours</th>
+        <th>Exercice précédent</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Vente des actifs</td>
+        <td>895645544</td>
+        <td>895645254</td>
+      </tr>
+      <tr>
+        <td>Loyers</td>
+        <td>895645544</td>
+        <td>895645254</td>
+      </tr>
+      <tr>
+        <td>Intérêts bancaires (crediteurs)</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Remboursement/Reversement TVA</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Recettes diversesS</td>
+        <td>5412632</td>
+        <td>5642632</td>
+      </tr>
+      <tr class="warning">
+      <td>Total</td>
+        <td>98652756</td>
+        <td>4512426</td>
+      </tr>
+  </table>
+  <br><br>
+  <center>
+  <h5>Note 7 : Dépenses Projet/Programme</h5>
+  </center>
+  <table class="table table-bordered" >
+    <thead>
+      <tr class="info">
+        <td></td>
+        <th>Total</th>
+        <th>Frais d'Exploitation Courants</th>
+        <th>Frais du Personnel</th>
+        <th>Dépenses Administratives</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="info">
+        <td>Localité 1</td>
         <td></td>
         <td></td>
         <td></td>
         <td></td>
+      </tr>
+      <tr>
+        <td>Assistance Familiale à Long Terme</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+      </tr>
+      <tr>
+        <td>Programmes d'Education</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+      </tr>
+      <tr>
+        <td>PRF</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
       </tr>      
       <tr class="success">
-        <td>Subventions du Gouuvernement</td>
+        <td>Total Localité 1</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+      </tr>
+      <tr>
+        <td>Des Fonds Restreints</td>
+        <td>-</td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+      <td>Des Fonds non Restreints</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr class="info">
+        <td>Localité 2</td>
         <td></td>
         <td></td>
         <td></td>
         <td></td>
       </tr>
       <tr>
-        <td>Localité 2 :</td>
+        <td>Assistance Familiale à Long Terme</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+      </tr>
+      <tr>
+        <td>Programmes d'Education</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+      </tr>
+      <tr>
+        <td>PRF</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+      </tr>      
+      <tr class="success">
+        <td>Total Localité 2</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+      </tr>
+      <tr>
+        <td>Des Fonds Restreints</td>
+        <td>-</td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+      <td>Des Fonds non Restreints</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr class="info">
+        <td>Localité 3</td>
         <td></td>
         <td></td>
         <td></td>
         <td></td>
       </tr>
       <tr>
-        <td>Structure 3</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td>0</td>
+        <td>Assistance Familiale à Long Terme</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
       </tr>
       <tr>
-        <td>Structure 4</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td>0</td>
-      </tr>
-      <tr class="warning">
-        <td>Total</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
+        <td>Programmes d'Education</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>-</td>
+        <td>(79825655)</td>
       </tr>
       <tr>
-        <td><br></td>
+        <td>PRF</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+      </tr>      
+      <tr class="success">
+        <td>Total Localité 3</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+      </tr>
+      <tr>
+        <td>Des Fonds Restreints</td>
+        <td>-</td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+      <td>Des Fonds non Restreints</td>
         <td></td>
         <td></td>
         <td></td>
         <td></td>
-      </tr>    
-      <tr class="warning">
-        <td>Total Fonds Restreints</td>
-        <td>0</td>
-        <td>85245295</td>
-        <td>7429525</td>
-        <td>8524562</td>
       </tr>  
+      <tr class="warning">
+        <td>Total des Dépenses</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+        <td>(79825655)</td>
+      </tr>
     </tbody>
   </table>
+  <br><br>
+  <center>
+  <h5>Note 8 : Dépenses Administratives (Bureau National) </h5>
+  </center>
+  <table class="table table-bordered" >
+    <thead>
+      <tr class="success">
+        <td></td>
+        <th>Exercice en cours</th>
+        <th>Exercice précédent</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Frais d'Exploitation Courants</td>
+        <td>895645544</td>
+        <td>895645254</td>
+      </tr>
+      <tr>
+        <td>Frais du Personnel</td>
+        <td>87451221</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Dépenses Administratives</td>
+        <td>5412632</td>
+        <td>5642632</td>
+      </tr>
+      <tr class="warning">
+      <td>Total</td>
+        <td>98652756</td>
+        <td>4512426</td>
+      </tr>
+  </table>
+  <br><br>
+  <center>
+  <h5>Note 9 : Dépenses de Collecte de Fonds (Fundraising)</h5>
+  </center>
+  <table class="table table-bordered" >
+    <thead>
+      <tr class="success">
+        <td></td>
+        <th>Exercice en cours</th>
+        <th>Exercice précédent</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Activités de Collecte de Fonds</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Frais d'Exploitation Courants</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Frais du Personnel</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Dépenses Administratives</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr class="warning">
+      <td>Total</td>
+        <td>98652756</td>
+        <td>4512426</td>
+      </tr>
+  </table>
+  <br><br>
+  <center>
+  <h5>Note 10 : Autres Dépenses</h5>
+  </center>
+  <table class="table table-bordered" >
+    <thead>
+      <tr class="success">
+        <td></td>
+        <th>Exercice en cours</th>
+        <th>Exercice précédent</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Amortissements</td>
+        <td>568756235</td>
+        <td>98653252</td>
+      </tr>
+      <tr>
+        <td>Dépenses liées à la vente des Actifs immobilises</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Autres évaluations & créances irrécouvrables</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Autres dépenses non monétaires</td>
+        <td>568756235</td>
+        <td>98653252</td>
+      </tr>
+      <tr class="warning">
+      <td>Total</td>
+        <td>98652756</td>
+        <td>4512426</td>
+      </tr>
+  </table>
+  <br><br>
+  <center>
+  <h5>Note 11 : Recettes/Dépenses des activités financières</h5>
+  </center>
+  <table class="table table-bordered" >
+    <thead>
+      <tr class="success">
+        <td></td>
+        <th>Exercice en cours</th>
+        <th>Exercice précédent</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Intérêts créditeurs générés par les dépôts</td>
+        <td>895645544</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Intérêts débiteurs générés par les emprunts</td>
+        <td>87451221</td>
+        <td>6525245</td>
+      </tr>
+      <tr>
+        <td>Gain ou perte de change (résultant de la variation des taux de change)</td>
+        <td>5412632</td>
+        <td>5642632</td>
+      </tr>
+      <tr class="warning">
+      <td>Total</td>
+        <td>98652756</td>
+        <td>4512426</td>
+      </tr>
+  </table>
+  <br><br>
+  <center>
+  <h5>Note 12 : Charges Fiscales</h5>
+  </center>
+  <table class="table table-bordered" >
+    <thead>
+      <tr class="success">
+        <td></td>
+        <th>Exercice en cours</th>
+        <th>Exercice précédent</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Impôts (ou taxes) exigibles sur les activités ordinaires</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>Taxe sur les subventions reçues par les ONG</td>
+        <td>-</td>
+        <td></td>
+      </tr>
+      <tr class="warning">
+      <td>Total</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+  </table>
+  <br><br>
 </div>
 
 </body>
