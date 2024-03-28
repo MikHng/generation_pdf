4838265b60fa601e2c743c57b440b8af6d866b0e
Alpha88 <aignondelofondimitri@gmail.com>
2024-03-28 10:15:30 +0100

    Migrationde students

---
+++ database/migrations/2024_03_28_091354_students.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('matricule')->nullable();
+            $table->string('name')->nullable();
+            $table->string('firstname')->nullable();
+            $table->string('notes')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
